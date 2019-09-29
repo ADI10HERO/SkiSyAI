e1bfc5bff99a71b1d9c0d11e8065f3cd84e0b7d7
Aditya Srivastava <adityasrivastava301199@gmail.com>
2019-09-29 14:05:00 +0530

    karchra theek

--- WebApp/adduser.py
+++ WebApp/adduser.py
@@ -8,7 +8,7 @@ engine = create_engine('sqlite:///skiai.db', echo=True)
 # create a Session
 Session = sessionmaker(bind=engine)
 session = Session()
-'''
+
 user = User("admin","password","Yes")
 session.add(user)
 
@@ -27,18 +27,7 @@ session.add(user)
 user = User("rj","8228","Yes")
 session.add(user)
 
- Data(fname,lname,gender,age,Historyofpresentillness,
-                    history1,history2,history3,history4,
-                    symptom1,symptom2,symptom3,symptom4,symptom5,
-                    Drugshistory,destination,model_pred,user_name,comment=None,status='0')
-'''
 
-case=Data('tanvi','shinde','female','22','present illness','Diabetes','-','-','-','Itching','-','-','-','-','drug history','/nothing','Acne','tanvi','None',0)
-session.add(case)
-case=Data('sansa','stark','female','22','present illness','Diabetes','-','-','-','Itching','-','-','-','-','drug history','/nothing','Tinea','sns','None',0)
-session.add(case)
-case=Data('arya','stark','female','22','present illness','Diabetes','-','-','-','Itching','-','-','-','-','drug history','/nothing','Tinea','ans','None',0)
-session.add(case)
 # commit the record the database
 session.commit()
 
---
+++ WebApp/createcase.py
@@ -0,0 +1,28 @@
+import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db import *
+
+engine = create_engine('sqlite:///skiai.db', echo=True)
+
+# create a Session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+'''
+# Data ka format
+Data(fname,lname,gender,age,Historyofpresentillness,
+                    history1,history2,history3,history4,
+                    symptom1,symptom2,symptom3,symptom4,symptom5,
+                    Drugshistory,destination,model_pred,user_name,comment=None,status='0')
+'''
+case=Data('tanvi','shinde','female','22','present illness','Diabetes','-','-','-','Itching','-','-','-','-','drug history','/nothing','Acne','tanvi','None',0)
+session.add(case)
+case=Data('sansa','stark','female','22','present illness','Diabetes','-','-','-','Itching','-','-','-','-','drug history','/nothing','Tinea','sns','None',0)
+session.add(case)
+case=Data('arya','stark','female','22','present illness','Diabetes','-','-','-','Itching','-','-','-','-','drug history','/nothing','Tinea','ans','None',0)
+session.add(case)
+
+session.commit()
+
+session.commit()
